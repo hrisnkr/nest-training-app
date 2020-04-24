e51f37eabbcce6fb6c0af992ba19e0aa9aa45115
Harisankar Thangavelu <harisankar.cs@hotmail.com>
2020-04-24 15:03:47 -0500

    Implemented getall, getitem, put methods

--- src/env/env.controller.ts
+++ src/env/env.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  NotFoundException,
+  Put,
+  Body,
+} from '@nestjs/common';
+import { EnvService } from './env.service';
 
 @Controller('env')
-export class EnvController {}
+export class EnvController {
+  constructor(private readonly envService: EnvService) {}
+
+  @Get()
+  public async getAll(): Promise<Record<string, string>> {
+    return this.envService.getALL();
+  }
+
+  @Get('/:id')
+  public async getEnv(@Param('id') id: string): Promise<string> {
+    const value = await this.envService.getEnv(id);
+    if (typeof value != 'string') {
+      throw new NotFoundException('ENV not found');
+    }
+    return value;
+  }
+
+  @Put('/:id')
+  public async setEnv(
+    @Param('id') id: string,
+    @Body('value') value: string,
+  ): Promise<string> {
+    const result = await this.envService.setEnv(id, value);
+
+    if (typeof result != 'string') {
+      throw new NotFoundException('ENV not found');
+    }
+    return result;
+  }
+}
